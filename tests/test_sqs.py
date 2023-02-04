13478ca12da23730f0476b9154110b50a2663930
Sanhe Hu <husanhe@gmail.com>
2023-02-04 00:57:01 -0500

    refactor

--- tests/test_sqs.py
+++ tests/test_sqs.py
@@ -1,34 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from aws_lambda_event.sqs import SQSEvent
+from aws_lambda_event.events.sqs import SQSEvent
 
 
 class TestSQSEvent:
-    data = {
-        "Records": [
-            {
-                "messageId": "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
-                "receiptHandle": "MessageReceiptHandle",
-                "body": "Hello from SQS!",
-                "attributes": {
-                    "ApproximateReceiveCount": "1",
-                    "SentTimestamp": "1523232000000",
-                    "SenderId": "123456789012",
-                    "ApproximateFirstReceiveTimestamp": "1523232000001"
-                },
-                "messageAttributes": {},
-                "md5OfBody": "{{{md5_of_body}}}",
-                "eventSource": "aws:sqs",
-                "eventSourceARN": "arn:aws:sqs:us-east-1:123456789012:MyQueue",
-                "awsRegion": "us-east-1"
-            }
-        ]
-    }
-
     def test(self):
-        event = SQSEvent(self.data)
-        _ = event.records[0]
+        event = SQSEvent.fake()
+        event_data = event.to_dict()
+        assert event.to_dict() == event_data
+
+        _ = event.Records[0]
 
 
 if __name__ == "__main__":
