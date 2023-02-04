3db73a961363c30d13b76778c17f3e210434cab9
Sanhe Hu <husanhe@gmail.com>
2023-02-04 01:01:46 -0500

    refactor

--- tests/test_kinesis_firehose.py
+++ tests/test_kinesis_firehose.py
@@ -1,26 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from aws_lambda_event.kinesis_firehose import KinesisFirehoseEvent
+from aws_lambda_event.events.kinesis_firehose import KinesisFirehoseEvent
 
 
 class TestKinesisFirehoseEvent:
-    data = {
-        "invocationId": "invocationIdExample",
-        "deliveryStreamArn": "arn:aws:kinesis:EXAMPLE",
-        "region": "us-east-1",
-        "records": [
-            {
-                "recordId": "49546986683135544286507457936321625675700192471156785154",
-                "approximateArrivalTimestamp": 1495072949453,
-                "data": "SGVsbG8sIHRoaXMgaXMgYSB0ZXN0IDEyMy4="
-            }
-        ]
-    }
-
     def test(self):
-        event = KinesisFirehoseEvent(self.data)
+        event = KinesisFirehoseEvent.fake()
+        event_data = event.to_dict()
+        assert event.to_dict() == event_data
+
         record = event.records[0]
+
         _ = record.binary_data
         _ = record.approximate_arrival_datetime
 
