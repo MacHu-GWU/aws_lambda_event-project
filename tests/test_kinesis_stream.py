d8f7bfb16dd92ac26e74de79eef0b7c4274292fe
Sanhe Hu <husanhe@gmail.com>
2023-02-04 01:07:39 -0500

    refactor

--- tests/test_kinesis_stream.py
+++ tests/test_kinesis_stream.py
@@ -1,34 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from aws_lambda_event.kinesis_stream import KinesisStreamEvent
+from aws_lambda_event.events.kinesis_stream import KinesisStreamEvent
 
 
 class TestKinesisFirehoseEvent:
-    data = {
-        "Records": [
-            {
-                "kinesis": {
-                    "partitionKey": "partitionKey-03",
-                    "kinesisSchemaVersion": "1.0",
-                    "data": "SGVsbG8sIHRoaXMgaXMgYSB0ZXN0IDEyMy4=",
-                    "sequenceNumber": "49545115243490985018280067714973144582180062593244200961",
-                    "approximateArrivalTimestamp": 1428537600
-                },
-                "eventSource": "aws:kinesis",
-                "eventID": "shardId-000000000000:49545115243490985018280067714973144582180062593244200961",
-                "invokeIdentityArn": "arn:aws:iam::EXAMPLE",
-                "eventVersion": "1.0",
-                "eventName": "aws:kinesis:record",
-                "eventSourceARN": "arn:aws:kinesis:EXAMPLE",
-                "awsRegion": "us-east-1"
-            }
-        ]
-    }
-
     def test(self):
-        event = KinesisStreamEvent(self.data)
-        record = event.records[0]
+        event = KinesisStreamEvent.fake()
+        event_data = event.to_dict()
+        assert event.to_dict() == event_data
+
+        record = event.Records[0]
+
         _ = record.kinesis_partition_key
         _ = record.kinesis_binary_data
         _ = record.kinesis_approximate_arrival_datetime
