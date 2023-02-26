0b33c1998006fddabf814fad5d2c8dfab8c4f1db
Sanhe Hu <husanhe@gmail.com>
2023-02-25 22:10:27 -0500

    add version in data faker

--- aws_lambda_event/events/cloudwatch_scheduled_event.py
+++ aws_lambda_event/events/cloudwatch_scheduled_event.py
@@ -41,6 +41,7 @@ class CloudWatchScheduledEvent(Base):
     def fake(cls) -> "CloudWatchScheduledEvent":
         data = {
             "id": "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
+            "version": "0",
             "detail-type": "Scheduled Event",
             "source": "aws.events",
             "account": "123456789012",
